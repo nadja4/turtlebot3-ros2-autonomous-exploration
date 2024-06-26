16d526c391ecc09564a36f0212fda51185289bc9
Nadja <nadja.lang@stud.hs-mannheim.de>
2024-06-26 23:08:58 +0200

    Implement obstacle detection instead of teleop

--- turtlebot3_teleop_test/turtlebot3_teleop_test/script/teleop_keyboard_test.py
+++ turtlebot3_teleop_test/turtlebot3_teleop_test/script/teleop_keyboard_test.py
@@ -48,189 +48,96 @@ else:
     import termios
     import tty
 
-BURGER_MAX_LIN_VEL = 0.22
-BURGER_MAX_ANG_VEL = 2.84
 
-WAFFLE_MAX_LIN_VEL = 0.26
-WAFFLE_MAX_ANG_VEL = 1.82
-
-LIN_VEL_STEP_SIZE = 0.01
-ANG_VEL_STEP_SIZE = 0.1
-
-TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
-
-msg = """
-Control Your TurtleBot3!
----------------------------
-Moving around:
-        w
-   a    s    d
-        x
-
-w/x : increase/decrease linear velocity (Burger : ~ 0.22, Waffle and Waffle Pi : ~ 0.26)
-a/d : increase/decrease angular velocity (Burger : ~ 2.84, Waffle and Waffle Pi : ~ 1.82)
-
-space key, s : force stop
-
-CTRL-C to quit
-"""
-
-e = """
-Communications Failed
-"""
+from geometry_msgs.msg import Twist
+from rclpy.node import Node
+from rclpy.qos import QoSProfile
+from rclpy.qos import qos_profile_sensor_data
+from sensor_msgs.msg import LaserScan
 
 
-def get_key(settings):
-    if os.name == 'nt':
-        return msvcrt.getch().decode('utf-8')
-    tty.setraw(sys.stdin.fileno())
-    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
-    if rlist:
-        key = sys.stdin.read(1)
-    else:
-        key = ''
+class Turtlebot3ObstacleDetection(Node):
 
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-    return key
+    def __init__(self):
+        super().__init__('turtlebot3_obstacle_detection')
 
+        """************************************************************
+        ** Initialise variables
+        ************************************************************"""
+        self.linear_velocity = 0.0  # unit: m/s
+        self.angular_velocity = 0.0  # unit: m/s
+        self.scan_ranges = []
+        self.init_scan_state = False  # To get the initial scan data at the beginning
 
-def print_vels(target_linear_velocity, target_angular_velocity):
-    print('currently:\tlinear velocity {0}\t angular velocity {1} '.format(
-        target_linear_velocity,
-        target_angular_velocity))
+        """************************************************************
+        ** Initialise ROS publishers and subscribers
+        ************************************************************"""
+        qos = QoSProfile(depth=10)
 
+        # Initialise publishers
+        self.cmd_vel_pub = self.create_publisher(Twist, 'cmd_vel', qos)
 
-def make_simple_profile(output, input, slop):
-    if input > output:
-        output = min(input, output + slop)
-    elif input < output:
-        output = max(input, output - slop)
-    else:
-        output = input
+        # Initialise subscribers
+        self.scan_sub = self.create_subscription(
+            LaserScan,
+            'scan',
+            self.scan_callback,
+            qos_profile=qos_profile_sensor_data)
+        self.cmd_vel_raw_sub = self.create_subscription(
+            Twist,
+            'cmd_vel_raw',
+            self.cmd_vel_raw_callback,
+            qos)
 
-    return output
+        """************************************************************
+        ** Initialise timers
+        ************************************************************"""
+        self.update_timer = self.create_timer(
+            0.010,  # unit: s
+            self.update_callback)
 
+        self.get_logger().info("Turtlebot3 obstacle detection node has been initialised.")
 
-def constrain(input_vel, low_bound, high_bound):
-    if input_vel < low_bound:
-        input_vel = low_bound
-    elif input_vel > high_bound:
-        input_vel = high_bound
-    else:
-        input_vel = input_vel
+    """*******************************************************************************
+    ** Callback functions and relevant functions
+    *******************************************************************************"""
+    def scan_callback(self, msg):
+        self.scan_ranges = msg.ranges
+        self.init_scan_state = True
 
-    return input_vel
+    def cmd_vel_raw_callback(self, msg):
+        self.linear_velocity = msg.linear.x
+        self.angular_velocity = msg.angular.z
 
+    def update_callback(self):
+        if self.init_scan_state is True:
+            self.detect_obstacle()
 
-def check_linear_limit_velocity(velocity):
-    if TURTLEBOT3_MODEL == 'burger':
-        return constrain(velocity, -BURGER_MAX_LIN_VEL, BURGER_MAX_LIN_VEL)
-    else:
-        return constrain(velocity, -WAFFLE_MAX_LIN_VEL, WAFFLE_MAX_LIN_VEL)
+    def detect_obstacle(self):
+        twist = Twist()
+        obstacle_distance = min(self.scan_ranges)
+        self.get_logger().info("Detect obstacle %s" %obstacle_distance)
+        safety_distance = 0.3  # unit: m
 
+        if obstacle_distance > safety_distance:
+            twist.linear.x = 0.02
+            twist.angular.z = 0.00
+        else:
+            twist.linear.x = 0.0
+            twist.angular.z = 0.0
+            self.get_logger().info("Obstacles are detected nearby. Robot stopped.")
 
-def check_angular_limit_velocity(velocity):
-    if TURTLEBOT3_MODEL == 'burger':
-        return constrain(velocity, -BURGER_MAX_ANG_VEL, BURGER_MAX_ANG_VEL)
-    else:
-        return constrain(velocity, -WAFFLE_MAX_ANG_VEL, WAFFLE_MAX_ANG_VEL)
-
+        self.cmd_vel_pub.publish(twist)
 
 def main():
-    settings = None
-    if os.name != 'nt':
-        settings = termios.tcgetattr(sys.stdin)
-
     rclpy.init()
 
-    qos = QoSProfile(depth=10)
-    node = rclpy.create_node('teleop_keyboard_test')
-    pub = node.create_publisher(Twist, 'cmd_vel', qos)
+    turtlebot3_obstacle_detection = Turtlebot3ObstacleDetection()
 
-    status = 0
-    target_linear_velocity = 0.0
-    target_angular_velocity = 0.0
-    control_linear_velocity = 0.0
-    control_angular_velocity = 0.0
+    rclpy.spin(turtlebot3_obstacle_detection)
 
-    try:
-        print(msg)
-        while(1):
-            key = get_key(settings)
-            if key == 'w':
-                target_linear_velocity =\
-                    check_linear_limit_velocity(target_linear_velocity + LIN_VEL_STEP_SIZE)
-                status = status + 1
-                print_vels(target_linear_velocity, target_angular_velocity)
-            elif key == 'x':
-                target_linear_velocity =\
-                    check_linear_limit_velocity(target_linear_velocity - LIN_VEL_STEP_SIZE)
-                status = status + 1
-                print_vels(target_linear_velocity, target_angular_velocity)
-            elif key == 'a':
-                target_angular_velocity =\
-                    check_angular_limit_velocity(target_angular_velocity + ANG_VEL_STEP_SIZE)
-                status = status + 1
-                print_vels(target_linear_velocity, target_angular_velocity)
-            elif key == 'd':
-                target_angular_velocity =\
-                    check_angular_limit_velocity(target_angular_velocity - ANG_VEL_STEP_SIZE)
-                status = status + 1
-                print_vels(target_linear_velocity, target_angular_velocity)
-            elif key == ' ' or key == 's':
-                target_linear_velocity = 0.0
-                control_linear_velocity = 0.0
-                target_angular_velocity = 0.0
-                control_angular_velocity = 0.0
-                print_vels(target_linear_velocity, target_angular_velocity)
-            else:
-                if (key == '\x03'):
-                    break
-
-            if status == 20:
-                print(msg)
-                status = 0
-
-            twist = Twist()
-
-            control_linear_velocity = make_simple_profile(
-                control_linear_velocity,
-                target_linear_velocity,
-                (LIN_VEL_STEP_SIZE / 2.0))
-
-            twist.linear.x = control_linear_velocity
-            twist.linear.y = 0.0
-            twist.linear.z = 0.0
-
-            control_angular_velocity = make_simple_profile(
-                control_angular_velocity,
-                target_angular_velocity,
-                (ANG_VEL_STEP_SIZE / 2.0))
-
-            twist.angular.x = 0.0
-            twist.angular.y = 0.0
-            twist.angular.z = control_angular_velocity
-
-            pub.publish(twist)
-
-    except Exception as e:
-        print(e)
-
-    finally:
-        twist = Twist()
-        twist.linear.x = 0.0
-        twist.linear.y = 0.0
-        twist.linear.z = 0.0
-
-        twist.angular.x = 0.0
-        twist.angular.y = 0.0
-        twist.angular.z = 0.0
-
-        pub.publish(twist)
-
-        if os.name != 'nt':
-            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-
+    turtlebot3_obstacle_detection.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
